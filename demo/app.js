f6ed76bff5b708ca835b3d2aa4cfce3f44993873
zxc0328 <120797887@qq.com>
2017-05-13 13:05:47 +0800

    Update demo

--- demo/app.js
+++ demo/app.js
@@ -2,39 +2,58 @@
 
 const Dilithium = require('../dilithium');
 
-class CounterButton extends Dilithium.Component {
+class TodoApp extends Dilithium.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      count: 0,
-      show: true
-    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {items: [], text: ''};
   }
-  clickHandler(){
-    let count = this.state.count
-    this.setState({show:false});
-  }
+
   render() {
     return (
       <div>
-        <h1>{this.props.title}</h1>
-        {this.state.show ? 
-          <div on-click={this.clickHandler.bind(this)}>
-            <span>Count:</span>
-            <span>{this.state.count}</span>
-          </div>
-          :
-          <div>
-            <span>FDF </span>
-          </div>
-        }
-        
+        <h3>TODO</h3>
+        <TodoList items={this.state.items} />
+        <form on-submit={this.handleSubmit}>
+          <input on-change={this.handleChange} value={this.state.text} />
+          <button>{'Add #' + (this.state.items.length + 1)}</button>
+        </form>
       </div>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({text: e.target.value});
+  }
+
+  handleSubmit(e) {
+    console.log("hhhh")
+    e.preventDefault();
+    var newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState({
+      text: " ",
+      items: this.state.items.concat(newItem)
+    });
+  }
+}
+
+class TodoList extends Dilithium.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     );
   }
 }
 
 Dilithium.render(
-    <CounterButton title="Hello React Rally!" />,
+    <TodoApp />,
     document.getElementById('container'),
   );
